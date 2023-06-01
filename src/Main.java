a916617047b7e2767824279aa52c3c5d8efcba6b
SarahDoma <108196951+SarahDoma@users.noreply.github.com>
2023-06-01 13:11:26 +0100

    Update Main.java; Withdraw UI function imported by Saratu

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 import UI.BankLoginUi;
+import UI.WithdrawUI;
 public class Main {
     public static void main(String[] args) {
         BankLoginUi bankLoginUi = new BankLoginUi();
         bankLoginUi.setVisible(true);
+        
+        double initialBalance = 100000.00;
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new WithdrawUI();
+            }
+        });
     }
 }
