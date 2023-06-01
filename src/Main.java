d7cf895a3142b738a84c11da33fe870fbdcad36b
SarahDoma <108196951+SarahDoma@users.noreply.github.com>
2023-06-01 23:07:49 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 import UI.BankLoginUi;
 import UI.WithdrawUI;
 import UI.DepositUI;
+import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         BankLoginUi bankLoginUi = new BankLoginUi();
         bankLoginUi.setVisible(true);
         
-        double initialBalance = 100000.00;
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 new WithdrawUI();
