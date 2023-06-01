5e2ffec876d6c0e18f9647d6c164acd5610ef1bf
loissyyy <130872200+loissyyy@users.noreply.github.com>
2023-06-01 23:16:32 +0100

    Update AccountTypeUI.java to Savings Frame: Deposit & Withdraw buttons added by Lois

--- src/UI/AccountTypeUI.java
+++ src/UI/AccountTypeUI.java
@@ -56,6 +56,8 @@ public class AccountTypeUI extends JFrame implements ActionListener {
     }
 }
 
+//Savings Frame start
+
 class SavingsFrame extends JFrame implements ActionListener {
 
     public JLabel headerLabel;
